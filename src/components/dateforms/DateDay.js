18310f6cd002fc06a7e2865e9fec519d15499241
Derwinesmitch <101403839+Derwinesmitch@users.noreply.github.com>
2023-04-19 11:00:59 +0200

    removed unused code

--- src/components/dateforms/DateDay.js
+++ src/components/dateforms/DateDay.js
@@ -3,7 +3,6 @@ import React from 'react'
 function DayBox ({NewDate, optionsDay, onClick}) {
 
 
-
 return (
     <div class="mr-1" onClick={onClick}>
         {new Intl.DateTimeFormat("en-US", optionsDay).format(NewDate)}
