f69f9033ac9fc6b7d111eefa8544f048d30922fc
Derwinesmitch <101403839+Derwinesmitch@users.noreply.github.com>
2023-04-19 11:01:32 +0200

    fixed useEffect

--- src/components/dateforms/DatePicker.js
+++ src/components/dateforms/DatePicker.js
@@ -1,27 +1,32 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import DatePicker from "react-datepicker";
 import '../../datepickerstyle.css';
 
+
+
+
 function Calendar ({changeDate, onShowCalendar, NewDate}) {
+
+
     const [startDate, setStartDate] = useState(NewDate);
     
+
     useEffect (()=> { 
       setStartDate(NewDate)
     },[NewDate])
 
     useEffect(()=>{
       changeDate(startDate)
-    },[startDate])
+        },[startDate, changeDate])
 
-    useEffect(()=> {
-      console.log("this is show calendar ",onShowCalendar)
+    useEffect(()=> {  
     },[onShowCalendar])
 
     return (
       <div className="hideInput"> 
         <DatePicker selected={startDate} onChange={(date) => {
+          
           setStartDate(date)
-          console.log(date)
           }}
           open={onShowCalendar}
           className="hideDatepicker"
